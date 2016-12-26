4e5e09a636927e5ffb440705e55a894cd37e2410
zhivodir <dzhivchik@gmail.com>
2016-12-26 15:45:31 +0200

    ordersMaker without ORM uncomplete

---
+++ src/main/java/ua/kiev/prog/Main.java
@@ -0,0 +1,10 @@
+package ua.kiev.prog;
+
+/**
+ * Created by User on 21.12.2016.
+ */
+public class Main {
+    public static void main(String[] args) {
+        DBConnect dbConnect = new DBConnect();
+    }
+}
---
+++ src/main/java/ua/kiev/prog/Operations.java
@@ -0,0 +1,125 @@
+package ua.kiev.prog;
+
+import com.mysql.jdbc.Statement;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Scanner;
+
+/**
+ * Created by User on 21.12.2016.
+ */
+public class Operations {
+    private DBConnect dbConnect;
+    private Scanner sc;
+
+    public Operations(DBConnect dbConnect) {
+        this.dbConnect = dbConnect;
+        this.sc = new Scanner(System.in);
+
+        while(true){
+            System.out.println();
+            System.out.println("Choice an operation:");
+            System.out.println("1.Add new client");
+            System.out.println("2.Add new good");
+            System.out.println("3.Create new order");
+            System.out.println("4.Cancel new order");
+
+            int choice = Integer.parseInt(sc.nextLine());
+            select(choice);
+        }
+    }
+
+    public void select(int choice){
+        switch (choice){
+            case 1:
+                addClient();
+                break;
+            case 2:
+                addGood();
+                break;
+            case 3:
+                addOrder();
+                break;
+            case 4:
+                System.out.println("Cancel order:");
+                int numOfRoom = Integer.parseInt(sc.nextLine());
+                break;
+            default:
+                break;
+        }
+    }
+
+    public void addClient(){
+        System.out.print("Enter a name of client: ");
+        String name = sc.nextLine();
+        try(PreparedStatement ps = dbConnect.getConnection().prepareStatement("INSERT INTO clients (name) VALUES (?)")){
+            ps.setString(1, name);
+            ps.executeUpdate();
+        }catch(SQLException e){e.printStackTrace();}
+    }
+
+    public void addGood(){
+        System.out.print("Enter a name of good: ");
+        String name = sc.nextLine();
+        System.out.print("Enter price of good: ");
+        double price = Double.parseDouble(sc.nextLine());
+        try(PreparedStatement ps = dbConnect.getConnection().prepareStatement("INSERT INTO goods (name, price) VALUES (?, ?)")){
+            ps.setString(1, name);
+            ps.setDouble(2, price);
+            ps.executeUpdate();
+        }catch(SQLException e){e.printStackTrace();}
+    }
+
+    public void addOrder(){
+        StringBuilder query = new StringBuilder();
+        query.append("INSERT INTO orders (id_client, date) VALUES");
+        int id_order = 0;
+        List<Integer> goods = new ArrayList<>();
+        System.out.print("Enter the client for order: ");
+        int id_client = Integer.parseInt(sc.nextLine());
+        try(PreparedStatement ps = dbConnect.getConnection().
+                prepareStatement(("INSERT INTO orders (id_client, date) VALUES (?, ?)"), Statement.RETURN_GENERATED_KEYS)){
+            ps.setInt(1, id_client);
+            ps.setDate(2, new java.sql.Date(new Date().getTime()));
+            ps.executeUpdate();
+            ResultSet rs = ps.getGeneratedKeys();
+            if (rs.next()) {
+                id_order = rs.getInt(1);
+            }
+        }catch(SQLException e){e.printStackTrace();}
+
+        while(true) {
+            System.out.println("Add new good into order(enter id): ");
+            String enter = sc.nextLine();
+            if(enter.equals("send")){
+                for(int i = 0; i < goods.size(); i++){
+                    query.append("(?, " + id_order + ")");
+                    if( i != goods.size() - 1){
+                        query.append(",");
+                    }
+                }
+                try(PreparedStatement ps = dbConnect.getConnection().prepareStatement(query.toString())){
+                    ps.setInt(1, id_client);
+                    ps.setDate(2, new java.sql.Date(new Date().getTime()));
+                    ps.executeUpdate();
+                    ResultSet rs = ps.getGeneratedKeys();
+                    if (rs.next()) {
+                        id_order = rs.getInt(1);
+                    }
+                }catch(SQLException e){e.printStackTrace();}
+            }else {
+                int id = Integer.parseInt(sc.nextLine());
+                goods.add(id);
+            }
+        }
+
+
+
+    }
+}
